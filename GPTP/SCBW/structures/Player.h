88ea70a1173e2ba9a5655d388ae860f676186e26
BoomerangAide <BoomerangAide@users.noreply.github.com>
2015-02-24 23:13:13 +0100

    Update Player.h
    Update with more advanced color related functions

--- GPTP/SCBW/structures/Player.h
+++ GPTP/SCBW/structures/Player.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "common.h"
 
+//Not like the advanced BWAPI class
 struct PLAYER {
 
   u32   id;
@@ -10,6 +11,20 @@ struct PLAYER {
   u8    force;
   char  name[25];
 
+  /// return the possibly non-standard color of the 
+  /// current player
   u8 getColor();
+  
+  /// return the possibly non-standard color of the 
+  /// player identified by PlayerId
+  static u8 getColor(u8 playerId);
+
+  /// return the faction (tribe, brood or other) color 
+  ///of current player
+  u8 getFactionColor();
+
+  /// return the faction (tribe, brood or other) color 
+  /// of the player identified by PlayerId
+  static u8 getFactionColor(u8 playerId);
 
 };
