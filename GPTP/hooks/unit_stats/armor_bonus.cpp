27361dbf0d904569bed844682b047d1a153d9f5f
BoomerangAide <BoomerangAide@users.noreply.github.com>
2015-02-25 11:32:44 +0100

    Update armor_bonus.cpp
    Remove using command

--- GPTP/hooks/unit_stats/armor_bonus.cpp
+++ GPTP/hooks/unit_stats/armor_bonus.cpp
@@ -10,18 +10,17 @@ namespace hooks {
 /// Returns the bonus armor for this unit.
 u8 getArmorBonusHook(const CUnit *unit) {
   //Default StarCraft behavior
-  using scbw::getUpgradeLevel;
 
   u8 armorUpg = 0;
   if (scbw::isBroodWarMode()) {
     if (unit->id == UnitId::Hero_Torrasque || unit->id == UnitId::ultralisk) {
       if ((units_dat::BaseProperty[unit->id] & UnitProperty::Hero)
-          || getUpgradeLevel(unit->playerId, UpgradeId::ChitinousPlating)) {
+          || scbw::getUpgradeLevel(unit->playerId, UpgradeId::ChitinousPlating)) {
         armorUpg = 2;
       }
     }
   }
-  return armorUpg + getUpgradeLevel(unit->playerId, units_dat::ArmorUpgrade[unit->id]);
+  return armorUpg + scbw::getUpgradeLevel(unit->playerId, units_dat::ArmorUpgrade[unit->id]);
 }
 
 }
