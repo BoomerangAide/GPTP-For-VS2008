506427059ed0834efcca776fb0f6a85febee92ae
UndeadStar <BoomerangAide@users.noreply.github.com>
2015-08-21 15:45:20 +0200

    Adding siege/tank mode hooks
    Adding siege/tank mode hooks

---
+++ GPTP/hooks/siege_transform.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "../SCBW/structures/CUnit.h"
+
+namespace hooks {
+
+	//go from Siege Mode to Tank Mode
+	void ordersTankMode(CUnit* unit);
+
+	//go from Tank Mode to Siege Mode
+	void ordersSiegeMode(CUnit* unit);
+
+	//The injector function that should be called in initialize.cpp
+	void injectSiegeTransformHooks();
+
+} //hooks
