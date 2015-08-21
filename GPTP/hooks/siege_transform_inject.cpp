ad783b1b410b1a11b03b6808128eb205170b115a
UndeadStar <BoomerangAide@users.noreply.github.com>
2015-08-21 15:45:48 +0200

    Adding siege/tank mode hooks
    Adding siege/tank mode hooks

---
+++ GPTP/hooks/siege_transform_inject.cpp
@@ -0,0 +1,52 @@
+//Injector source file for the Siege Transformation hook module.
+#include "siege_transform.h"
+#include <hook_tools.h>
+
+//Inject with jmpPatch()
+void __declspec(naked) unitTransformIntoTankModeOrder() {
+
+	static CUnit *unitTransforming; 
+
+	__asm {
+		PUSHAD
+		MOV EBP, ESP
+		MOV unitTransforming, EAX
+	}
+
+	hooks::ordersTankMode(unitTransforming);
+
+	__asm {
+		POPAD
+		RETN
+	}
+
+}
+
+//Inject with jmpPatch()
+void __declspec(naked) unitTransformIntoSiegeModeOrder() {
+
+	static CUnit *unitTransforming; 
+
+	__asm {
+		PUSHAD
+		MOV EBP, ESP
+		MOV unitTransforming, EDI
+	}
+
+	hooks::ordersSiegeMode(unitTransforming);
+
+	__asm {
+		POPAD
+		RETN
+	}
+
+}
+
+namespace hooks {
+
+	void injectSiegeTransformHooks() {
+		jmpPatch(unitTransformIntoTankModeOrder,	0x00464AE0);
+		jmpPatch(unitTransformIntoSiegeModeOrder,	0x00464BD0);
+	}
+
+}
