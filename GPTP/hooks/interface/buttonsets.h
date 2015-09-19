2616be918e9d78118d6f6bfde09fb9507b35ad05
UndeadStar <BoomerangAide@users.noreply.github.com>
2015-09-19 21:43:03 +0200

    Update buttonsets.h
    Fixing updateButtonSet incorrect jmp behavior.
    Re-adding updateCurrentButtonset (was commented out)

--- GPTP/hooks/interface/buttonsets.h
+++ GPTP/hooks/interface/buttonsets.h
@@ -5,9 +5,10 @@
 
 namespace hooks {
 
-void updateButtonSetEx();																						//00458BC0  updateButtonSetEx
-void updateButtonSet_Sub458D50();																				//00458D50  sub_458D50
-bool updateButtonSet();																							//00458DE0  updateButtonSet
+void updateButtonSetEx();			//00458BC0  updateButtonSetEx
+void updateButtonSet_Sub458D50();	//00458D50  sub_458D50
+void updateButtonSet();				//00458DE0  updateButtonSet
+void updateCurrentButtonset();		//004599A0  updateCurrentButtonset
 
 BUTTON_SET* getButtonSet(int index);
 
