2aaa6be37cc64a3cb95814d300065b29cabd2292
UndeadStar <BoomerangAide@users.noreply.github.com>
2015-12-18 14:59:15 +0100

    Add hooked function 4591D0
    Add hooked function 4591D0 (may be one significant requirement for more than 9 buttons)

--- GPTP/hooks/interface/buttonsets.h
+++ GPTP/hooks/interface/buttonsets.h
@@ -8,6 +8,7 @@ namespace hooks {
 void updateButtonSetEx();			//00458BC0  updateButtonSetEx
 void updateButtonSet_Sub458D50();	//00458D50  sub_458D50
 void updateButtonSet();				//00458DE0  updateButtonSet
+void updateButtonSet_Sub4591D0();	//004591D0  sub_4591D0
 void updateCurrentButtonset();		//004599A0  updateCurrentButtonset
 
 BUTTON_SET* getButtonSet(int index);
