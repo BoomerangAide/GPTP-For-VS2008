4e0f2d7e4ddbf0d0f14b419cdae2903913263a7a
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-05-03 13:50:35 +0200

    minor changes
    -orderComputer_cl moved to CUnit
    -units_dat element name changed from "SpecialAngle" to "StartDirection"
    -added ; for presentation of collapsed helpers

--- GPTP/hooks/orders/siege_transform.cpp
+++ GPTP/hooks/orders/siege_transform.cpp
@@ -11,7 +11,6 @@ bool getCloserToDestination_sub402BE0(CUnit *unit);									//0x00402BE0
 void orderReturnToIdle(CUnit *unit);												//0x00463770
 bool checkForCloseUnit_sub464290(CUnit *unit);										//0x00464290
 void playSubunitIscript(CUnit *unit, IscriptAnimation::Enum anim);					//0x00474380
-void orderComputer_cl(CUnit *unit, u8 orderId); 									//0x00475310
 void actUnitReturnToIdle(CUnit *unit);												//0x00475420
 void replaceUnitWithType(CUnit *unit, u16 newUnitId);								//0x0049FED0
 void makeToHoldPosition(CUnit *unit);												//0x004EB5B0
@@ -32,7 +31,7 @@ namespace hooks {
 				if(!(unit->subunit->status & UnitStatus::NoBrkCodeStart)) {
 
 					unit->subunit->orderTo(OrderId::Nothing3,unit);
-					unitSetRetreatPoint(unit->subunit, units_dat::SpecialAngle[unit->subunit->id]);
+					unitSetRetreatPoint(unit->subunit, units_dat::StartDirection[unit->subunit->id]);
 					playSubunitIscript(unit,IscriptAnimation::SpecialState1);
 					unit->mainOrderState = 2;
 
@@ -123,9 +122,9 @@ namespace hooks {
 
 					if( !(unit->subunit->status & UnitStatus::NoBrkCodeStart) ) {
 
-						unitSetRetreatPoint(unit,units_dat::SpecialAngle[unit->id]);
+						unitSetRetreatPoint(unit,units_dat::StartDirection[unit->id]);
 						unit->subunit->orderTo(OrderId::Nothing3,unit);
-						unitSetRetreatPoint(unit->subunit,units_dat::SpecialAngle[unit->subunit->id]);
+						unitSetRetreatPoint(unit->subunit,units_dat::StartDirection[unit->subunit->id]);
 						playSubunitIscript(unit,IscriptAnimation::SpecialState1);
 						unit->mainOrderState = 2;
 
@@ -173,7 +172,7 @@ namespace hooks {
 
 					if( !(unit->orderQueueHead->orderId == OrderId::WatchTarget) ) {
 
-						orderComputer_cl(unit,units_dat::ReturnToIdleOrder[unit->id]);
+						unit->orderComputerCL(units_dat::ReturnToIdleOrder[unit->id]);
 						actUnitReturnToIdle(unit);
 						orderReturnToIdle(unit->subunit);
 						bStopThere = true;
@@ -203,15 +202,17 @@ const u32 Func_UnitSetRetreatPoint = 0x004011A0;
 //angleValue is a guess
 void unitSetRetreatPoint(CUnit *unit, u8 angleValue) {
 
-  __asm {
-    PUSHAD
-	MOV AL, angleValue
-	MOV EDX, unit
-    CALL Func_UnitSetRetreatPoint
-    POPAD
-  }
+	__asm {
+		PUSHAD
+		MOV AL, angleValue
+		MOV EDX, unit
+		CALL Func_UnitSetRetreatPoint
+		POPAD
+	}
 
 }
+
+;
 
 const u32 Func_Sub402BE0_GetCloserToDestination = 0x00402BE0;
 //return true when unit reached nextTargetWaypoint either at call
@@ -223,45 +224,52 @@ bool getCloserToDestination_sub402BE0(CUnit *unit) {
 
 	static Bool32 bResult;
 
-  __asm {
-    PUSHAD
-	MOV ESI, unit
-    CALL Func_Sub402BE0_GetCloserToDestination
-	MOV bResult, EAX
-	POPAD
-  }
+	__asm {
+		PUSHAD
+		MOV ESI, unit
+		CALL Func_Sub402BE0_GetCloserToDestination
+		MOV bResult, EAX
+		POPAD
+	}
 
-  return (bResult == 1);
+	return (bResult == 1);
 
 }
+
+;
 
 const u32 Func_OrderReturnToIdle = 0x00463770;
 void orderReturnToIdle(CUnit *unit) {
 
-  __asm {
-    PUSHAD
-	MOV ESI, unit
-    CALL Func_OrderReturnToIdle
-    POPAD
-  }
+	__asm {
+		PUSHAD
+		MOV ESI, unit
+		CALL Func_OrderReturnToIdle
+		POPAD
+	}
 
 }
+
+;
 
 const u32 Func_Sub464290_CheckForCloseUnit = 0x00464290;
 bool checkForCloseUnit_sub464290(CUnit *unit) {
 
 	static Bool32 bResult;
 
-  __asm {
-    PUSHAD
-	MOV EAX, unit
-    CALL Func_Sub464290_CheckForCloseUnit
-	MOV bResult, EAX
-	POPAD
-  }
+	__asm {
+		PUSHAD
+		MOV EAX, unit
+		CALL Func_Sub464290_CheckForCloseUnit
+		MOV bResult, EAX
+		POPAD
+	}
 
-  return (bResult == 1);
+	return (bResult == 1);
+
 }
+
+;
 
 const u32 Func_PlaySubunitIscript = 0x00474380;
 void playSubunitIscript(CUnit *unit, IscriptAnimation::Enum anim) {
@@ -270,38 +278,28 @@ void playSubunitIscript(CUnit *unit, IscriptAnimation::Enum anim) {
 
 	assert(unit->subunit);
 
-  __asm {
-    PUSHAD
-	MOV EBX, unit
-	PUSH anim_
-    CALL Func_PlaySubunitIscript
-    POPAD
-  }
+	__asm {
+		PUSHAD
+		MOV EBX, unit
+		PUSH anim_
+		CALL Func_PlaySubunitIscript
+		POPAD
+	}
 
 }
 
-const u32 Func_OrderComputer_cl = 0x00475310;
-void orderComputer_cl(CUnit *unit, u8 orderId) {
-
-  __asm {
-    PUSHAD
-    MOV CL, orderId
-    MOV ESI, unit
-    CALL Func_OrderComputer_cl
-    POPAD
-  }
-
-}
+;
 
 const u32 Func_ActUnitReturnToIdle = 0x00475420;
 void actUnitReturnToIdle(CUnit *unit) {
 
-  __asm {
-    PUSHAD
-    MOV EAX, unit
-    CALL Func_ActUnitReturnToIdle
-    POPAD
-  }
+	__asm {
+		PUSHAD
+		MOV EAX, unit
+		CALL Func_ActUnitReturnToIdle
+		POPAD
+	}
+
 }
 
 
@@ -310,25 +308,26 @@ void replaceUnitWithType(CUnit *unit, u16 newUnitId) {
 
 	u32 newUnitId_ = newUnitId;
 
-  __asm {
-    PUSHAD
-    PUSH newUnitId_
-    MOV EAX, unit
-    CALL Func_ReplaceUnitWithType
-    POPAD
-  }
+	__asm {
+		PUSHAD
+		PUSH newUnitId_
+		MOV EAX, unit
+		CALL Func_ReplaceUnitWithType
+		POPAD
+	}
 
 }
 
 const u32 Func_OrdersHoldPositionSuicidal = 0x004EB5B0;
 void makeToHoldPosition(CUnit *unit) {
 
-  __asm {
-    PUSHAD
-    MOV ESI, unit
-    CALL Func_OrdersHoldPositionSuicidal
-    POPAD
-  }
+	__asm {
+		PUSHAD
+		MOV ESI, unit
+		CALL Func_OrdersHoldPositionSuicidal
+		POPAD
+	}
+
 }
 
 } //Unnamed namespace
