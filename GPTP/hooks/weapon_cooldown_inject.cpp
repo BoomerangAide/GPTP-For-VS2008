f73a370381de5eb871c94836601c5e7019a8904b
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-05-03 14:02:51 +0200

    minor fixes
    rewriting some parts to make sure it work (more) as expected

--- GPTP/hooks/weapon_cooldown_inject.cpp
+++ GPTP/hooks/weapon_cooldown_inject.cpp
@@ -1,29 +1,27 @@
 #include "weapon_cooldown.h"
 #include "../hook_tools.h"
 
-//V241 for VS2008
-
 namespace {
 
 //Inject with jmpPatch()
 const u32 Hook_GetModifiedWeaponCooldown  = 0x00475DC0;
 void __declspec(naked) getModifiedWeaponCooldownWrapper() {
-	static CUnit *unit;
+
+	static CUnit* unit;
 	static u8 weaponId;
 	static u32 cooldown;
+
 	__asm {
+		MOV unit, ESI
+		MOV weaponId, AL
 		PUSHAD
-		MOV ebp, esp
-		MOV unit, esi
-		MOV weaponId, al
 	}
 
   cooldown = hooks::getModifiedWeaponCooldownHook(unit, weaponId);
 
 	__asm {
-		MOV eax, cooldown
-		MOV [esp + 28], eax
 		POPAD
+		MOV EAX, cooldown
 		RETN
 	}
 }
@@ -33,7 +31,7 @@ void __declspec(naked) getModifiedWeaponCooldownWrapper() {
 namespace hooks {
 
 void injectWeaponCooldownHook() {
-  jmpPatch(getModifiedWeaponCooldownWrapper, Hook_GetModifiedWeaponCooldown);
+  jmpPatch(getModifiedWeaponCooldownWrapper, Hook_GetModifiedWeaponCooldown, 2);
 }
 
 } //hooks
