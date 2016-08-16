53a0ea3720755589d9c78dc5b8b8680ff62769cc
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-08-16 15:08:41 +0200

    Update 3.999

--- GPTP/hooks/orders/infestation.h
+++ GPTP/hooks/orders/infestation.h
@@ -3,14 +3,14 @@
 
 namespace hooks {
 
-	bool isInfestableUnit(const CUnit *unit);
-	bool unitCanInfest(const CUnit *unit);
+	bool isInfestableUnit(CUnit* unit);
+	bool unitCanInfest(CUnit* unit);
 
 	//infestation order used by infestation victim
-	void orderMorphIntoInfested(CUnit *unitInfested);
+	void orderMorphIntoInfested(CUnit* unitInfested);
 
 	//infestation order used by the unit that infest
-	void orderInfestTarget(CUnit *unitInfesting);
+	void orderInfestTarget(CUnit* unitInfesting);
 	
 	//The injector function that should be called in initialize.cpp
 	void injectInfestationHooks();
