dd3ae24e73d5ee6b91c39e1173d5ecc76be58416
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-08-16 14:53:07 +0200

    Update 3.999

--- GPTP/hooks/interface/weapon_armor_tooltip_inject.cpp
+++ GPTP/hooks/interface/weapon_armor_tooltip_inject.cpp
@@ -8,45 +8,48 @@ namespace {
 const u32 Func_CreateContextHelp = 0x00481510;
 //Identical to function @ 0x00457350
 void createContextHelpFromDialog(BinDlg* dialog, const char *text) {
-  u32 x = dialog->bounds.right, y = dialog->bounds.bottom;
 
-  if (dialog->controlType != 0) {
-    x += dialog->parent->bounds.left;
-    y += dialog->parent->bounds.top;
-  }
+	u32 x = dialog->bounds.right, y = dialog->bounds.bottom;
 
-  __asm {
-    PUSHAD
-    PUSH text
-    PUSH y
-    MOV EAX, x
-    CALL Func_CreateContextHelp
-    POPAD
-  }
+	if (dialog->controlType != 0) {
+		x += dialog->parent->bounds.left;
+		y += dialog->parent->bounds.top;
+	}
+
+	__asm {
+		PUSHAD
+		PUSH text
+		PUSH y
+		MOV EAX, x
+		CALL Func_CreateContextHelp
+		POPAD
+	}
+
 }
 
 //Replaces function @ 0x004576A0
 void __stdcall statdatalUserInteractWrapper(BinDlg *consoleIcon) {
-  GuiOverlay *gui = (GuiOverlay*) consoleIcon->user;
 
-  switch (gui->overlayType) {
-    case 0:
-      createContextHelpFromDialog(consoleIcon,
-        hooks::getWeaponTooltipString((u8) gui->id, *activePortraitUnit));
-      break;
-    case 1:
-      createContextHelpFromDialog(consoleIcon,
-        hooks::getArmorTooltipString(*activePortraitUnit));
-      break;
-    case 2:
-      createContextHelpFromDialog(consoleIcon,
-        hooks::getShieldTooltipString(*activePortraitUnit));
-      break;
-    case 3:
-      createContextHelpFromDialog(consoleIcon,
-        hooks::getSpecialTooltipString(gui->id, *activePortraitUnit));
-      break;
-  }
+	GuiOverlay *gui = (GuiOverlay*) consoleIcon->user;
+
+	switch (gui->overlayType) {
+		case 0:
+			createContextHelpFromDialog(consoleIcon,
+				hooks::getWeaponTooltipString((u8) gui->id, *activePortraitUnit));
+			break;
+		case 1:
+			createContextHelpFromDialog(consoleIcon,
+				hooks::getArmorTooltipString(*activePortraitUnit));
+			break;
+		case 2:
+			createContextHelpFromDialog(consoleIcon,
+				hooks::getShieldTooltipString(*activePortraitUnit));
+			break;
+		case 3:
+			createContextHelpFromDialog(consoleIcon,
+				hooks::getSpecialTooltipString(gui->id, *activePortraitUnit));
+			break;
+	}
 }
 
 } //unnamed namespace
@@ -55,7 +58,7 @@ void __stdcall statdatalUserInteractWrapper(BinDlg *consoleIcon) {
 namespace hooks {
 
 void injectUnitTooltipHook() {
-  callPatch(&statdatalUserInteractWrapper, 0x00457D4B);
+	callPatch(&statdatalUserInteractWrapper, 0x00457D4B);
 }
 
 } //hooks
