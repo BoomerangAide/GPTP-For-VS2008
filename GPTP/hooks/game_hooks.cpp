10b74888f000643b99ffdc0dca3ab9e8f6055057
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-08-17 13:16:44 +0200

    Update 3.999

--- GPTP/hooks/game_hooks.cpp
+++ GPTP/hooks/game_hooks.cpp
@@ -3,7 +3,6 @@
 #include "game_hooks.h"
 #include <graphics/graphics.h>
 #include <SCBW/api.h>
-#include <SCBW/scbwdata.h>
 #include <SCBW/ExtendSightLimit.h>
 #include "psi_field.h"
 #include <cstdio>
@@ -14,34 +13,37 @@ namespace hooks {
 /// This hook is called every frame; most of your plugin's logic goes here.
 bool nextFrame() {
 
-  if (!scbw::isGamePaused()) { //If the game is not paused
+	if (!scbw::isGamePaused()) { //If the game is not paused
 
-    scbw::setInGameLoopState(true); //Needed for scbw::random() to work
-    graphics::resetAllGraphics();
-    hooks::updatePsiFieldProviders();
+		scbw::setInGameLoopState(true); //Needed for scbw::random() to work
+		graphics::resetAllGraphics();
+		hooks::updatePsiFieldProviders();
     
-    //This block is executed once every game.
-    if (*elapsedTimeFrames == 0) {
-      //Write your code here
-      scbw::printText(PLUGIN_NAME ": Hello, world!");
-    }
+		//This block is executed once every game.
+		if (*elapsedTimeFrames == 0) {
+			//Write your code here
+			scbw::printText(PLUGIN_NAME ": Hello, world!");
+		}
 
-    //Loop through all visible units in the game.
-    for (CUnit *unit = *firstVisibleUnit; unit; unit = unit->link.next) {
-      //Write your code here
-    }
+		//Loop through all visible units in the game.
+		for (CUnit *unit = *firstVisibleUnit; unit; unit = unit->link.next) {
+			//Write your code here
+		}
 
-    scbw::setInGameLoopState(false);
-  }
-  return true;
+		scbw::setInGameLoopState(false);
+		
+	}
+  
+	return true;
+  
 }
 
 bool gameOn() {
-  return true;
+	return true;
 }
 
 bool gameEnd() {
-  return true;
+	return true;
 }
 
 } //hooks
