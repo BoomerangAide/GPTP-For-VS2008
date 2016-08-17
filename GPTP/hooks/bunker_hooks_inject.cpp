9e56449c121897678b714d7a8c7219f47ab0d0b6
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-08-17 13:03:03 +0200

    Update 3.999

--- GPTP/hooks/bunker_hooks_inject.cpp
+++ GPTP/hooks/bunker_hooks_inject.cpp
@@ -1,59 +1,55 @@
 #include "bunker_hooks.h"
 #include "../hook_tools.h"
 
-//V241 for VS2008
-
 namespace {
 
 const u32 Hook_UnitCanAttackInsideBunkerYes = 0x004790DC;
 void __declspec(naked) unitCanAttackInsideBunkerWrapper() {
-  CUnit *unit;
-  __asm {
-    PUSHAD
-    MOV EBP, ESP
-    MOV unit, ESI
-  }
 
-  if (hooks::unitCanAttackInsideBunkerHook(unit)) {
-    __asm {
-      POPAD
-      JMP Hook_UnitCanAttackInsideBunkerYes
-    }
-  }
-  else {
-    __asm {
-      POPAD
-      POP ESI   ;//Code at 0x00479147
-      RETN
-    }
-  }
+	static CUnit* unit;
+
+	__asm {
+		MOV unit, EAX
+		PUSHAD
+	}
+
+	hooks::unitAttackFromInsideBunkerHook(unit);
+
+	__asm {
+		POPAD
+		RETN
+	}
+
 }
+
+;
 
 void __declspec(naked) createBunkerAttackThingyWrapper() {
-  static CUnit *unit;
-  __asm {
-    PUSHAD
-    MOV EBP, ESP
-    MOV unit, EAX
-  }
 
-  hooks::createBunkerAttackThingyHook(unit);
+	static CUnit* unit;
 
-  __asm {
-    POPAD
-    RETN
-  }
+	__asm {
+		MOV unit, EAX
+		PUSHAD
+	}
+
+	hooks::createBunkerAttackThingyHook(unit);
+
+	__asm {
+		POPAD
+		RETN
+	}
 }
 
-
+;
 
 } //unnamed namespace
 
 namespace hooks {
 
 void injectBunkerHooks() {
-  jmpPatch(unitCanAttackInsideBunkerWrapper, 0x004790A3);
-  callPatch(createBunkerAttackThingyWrapper, 0x00478CAF);
+  jmpPatch(unitCanAttackInsideBunkerWrapper,	0x004790A0, 2);
+  jmpPatch(createBunkerAttackThingyWrapper,		0x00477FD0, 3);
 }
 
 } //hooks
