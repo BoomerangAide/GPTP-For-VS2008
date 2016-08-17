c79e8666a50a20d0b08e722bb33285b9282c8af2
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-08-17 13:05:09 +0200

    Update 3.999

--- GPTP/hooks/cloak_nearby_units_inject.cpp
+++ GPTP/hooks/cloak_nearby_units_inject.cpp
@@ -1,28 +1,32 @@
 #include "cloak_nearby_units.h"
 #include <hook_tools.h>
 
-//V241 for VS2008
-
 void __declspec(naked) cloakNearbyUnitsWrapper() {
-  CUnit *unit;
-  __asm {
-    PUSHAD
-    MOV EBP, ESP
-    MOV unit, EDI 
-  }
 
-  hooks::cloakNearbyUnitsHook(unit);
+	static CUnit* unit;
 
-  __asm {
-    POPAD
-    RETN
-  }
+	__asm {
+		PUSH EBP
+		MOV EBP, ESP
+		MOV unit, EDI
+		PUSHAD
+	}
+
+	hooks::cloakNearbyUnitsHook(unit);
+
+	__asm {
+		POPAD
+		MOV ESP, EBP
+		POP EBP
+		RETN
+	}
+
 }
 
 namespace hooks {
 
 void injectCloakNearbyUnits() {
-  jmpPatch(cloakNearbyUnitsWrapper, 0x00491C20);
+	jmpPatch(cloakNearbyUnitsWrapper, 0x00491C20);
 }
 
 } //hooks
