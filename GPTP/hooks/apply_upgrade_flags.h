8d2ad5c4263492d326e04c7e6c08c0fa0b8468d7
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-08-17 12:54:36 +0200

    Update 3.999

--- GPTP/hooks/apply_upgrade_flags.h
+++ GPTP/hooks/apply_upgrade_flags.h
@@ -4,17 +4,15 @@
 #pragma once
 #include <SCBW/structures.h>
 
-//V241 for VS2008
-
 namespace hooks {
 
-void applyUpgradeFlagsToNewUnitHook(CUnit *unit);
-void applyUpgradeFlagsToExistingUnitsHook(u8 playerId, u8 upgradeId);
+void applyUpgradeFlagsToNewUnitHook(CUnit* unit);
+void applyUpgradeFlagsToExistingUnitsHook(CUnit* unit, u8 upgradeId);
 
 void injectApplyUpgradeFlags();
 
 //Wrapper for the native version of applyUpgradeFlagsToExistingUnits().
 //Applies @upgradeId to all units that are owned by the owner of @p unit.
-void applyUpgradeFlagsToExistingUnits(CUnit *unit, u8 upgradeId);
+void applyUpgradeFlagsToExistingUnits(CUnit* unit, u8 upgradeId);
 
 } //hooks
