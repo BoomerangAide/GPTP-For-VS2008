bd39bc34cb2c648e0b645509da6c4ada4f336703
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-08-17 12:58:08 +0200

    Update 3.999

--- GPTP/hooks/attack_priority_inject.cpp
+++ GPTP/hooks/attack_priority_inject.cpp
@@ -6,63 +6,75 @@ namespace {
 const u32 Hook_GetAttackPriority = 0x00442160;
 //Inject with jmpPatch()
 void __declspec(naked) getAttackPriorityWrapper() {
-  static CUnit *target, *attacker;
-  static u32 result;
 
-  __asm {
-    PUSHAD
-    MOV EBP, ESP
-    MOV target, EDI
-    MOV attacker, EBX
-  }
+	static CUnit* target, *attacker;
+	static u32 result;
 
-  result = hooks::getAttackPriorityHook(target, attacker);
+	__asm {
+		MOV target, EDI
+		MOV attacker, EBX
+		PUSHAD
+	}
 
-  __asm {
-    POPAD
-    MOV EAX, result
-    RETN
-  }
+	result = hooks::getAttackPriorityHook(target, attacker);
+
+	__asm {
+		POPAD
+		MOV EAX, result
+		RETN
+	}
+
 }
 
 const u32 Hook_FindBestAttackTarget = 0x00443080;
 //Inject with jmpPatch()
 void __declspec(naked) findBestAttackTargetWrapper() {
-  static CUnit *unit;
-  static const CUnit *target;
 
-  __asm {
-    PUSHAD
-    MOV unit, EAX
-  }
+	static CUnit* unit;
+	static CUnit* target;
 
-  target = hooks::findBestAttackTargetHook(unit);
+	__asm {
+		PUSH EBP
+		MOV EBP, ESP
+		MOV unit, EAX
+		PUSHAD
+	}
 
-  __asm {
-    POPAD
-    MOV EAX, target
-    RETN
-  }
+	target = hooks::findBestAttackTargetHook(unit);
+
+	__asm {
+		POPAD
+		MOV EAX, target
+		MOV ESP, EBP
+		POP EBP
+		RETN
+	}
+
 }
 
 const u32 Hook_FindRandomAttackTarget = 0x00442FC0;
 //Inject with jmpPatch()
 void __declspec(naked) findRandomAttackTargetWrapper() {
-  static CUnit *unit;
-  static const CUnit *target;
 
-  __asm {
-    PUSHAD
-    MOV unit, ESI
-  }
+	static CUnit* unit;
+	static CUnit* target;
 
-  target = hooks::findRandomAttackTargetHook(unit);
+	__asm {
+		PUSH EBP
+		MOV EBP, ESP
+		MOV unit, ESI
+		PUSHAD
+	}
 
-  __asm {
-    POPAD
-    MOV EAX, target
-    RETN
-  }
+	target = hooks::findRandomAttackTargetHook(unit);
+
+	__asm {
+		POPAD
+		MOV EAX, target
+		MOV ESP, EBP
+		POP EBP
+		RETN
+	}
 }
 
 } //unnamed namespace
@@ -70,10 +82,9 @@ void __declspec(naked) findRandomAttackTargetWrapper() {
 namespace hooks {
 
 void injectAttackPriorityHooks() {
-  jmpPatch(getAttackPriorityWrapper,      Hook_GetAttackPriority);
-  jmpPatch(findBestAttackTargetWrapper,   Hook_FindBestAttackTarget);
-  jmpPatch(findRandomAttackTargetWrapper, Hook_FindRandomAttackTarget);
+	jmpPatch(getAttackPriorityWrapper,		Hook_GetAttackPriority,			3);
+	jmpPatch(findBestAttackTargetWrapper,	Hook_FindBestAttackTarget,		1);
+	jmpPatch(findRandomAttackTargetWrapper,	Hook_FindRandomAttackTarget,	1);
 }
 
 } //hooks
-
