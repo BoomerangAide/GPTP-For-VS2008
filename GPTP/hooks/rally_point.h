9e4290314ef4f31a3d59c7fc8c9ed2350f1cd0ce
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-08-17 13:23:58 +0200

    Update 3.999

--- GPTP/hooks/rally_point.h
+++ GPTP/hooks/rally_point.h
@@ -1,13 +1,11 @@
 #pragma once
 #include "../SCBW/structures/CUnit.h"
 
-//V241 for VS2008
-
 namespace hooks {
 
-void orderNewUnitToRally(CUnit* unit, CUnit* factory);
-void setRallyPosition(CUnit* unit, u16 x, u16 y);
-void setRallyUnit(CUnit* unit, CUnit* target);
+void setRallyUnit(CUnit* unit, CUnit* target);			//0x00466B40
+void orderNewUnitToRally(CUnit* unit, CUnit* factory);	//0x00466F50
+void setRallyPosition(CUnit* unit, u16 x, u16 y);		//0x00466910
 
 void injectRallyHooks();
 
