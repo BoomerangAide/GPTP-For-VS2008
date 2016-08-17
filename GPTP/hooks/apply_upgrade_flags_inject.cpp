7c032339476a131b4f26e594313e224d40fe0ca8
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-08-17 12:55:14 +0200

    Update 3.999

--- GPTP/hooks/apply_upgrade_flags_inject.cpp
+++ GPTP/hooks/apply_upgrade_flags_inject.cpp
@@ -2,43 +2,54 @@
 #include "apply_upgrade_flags.h"
 #include <hook_tools.h>
 
-//V241 for VS2008
-
 namespace {
 
 void __declspec(naked) applyUpgradeFlagsToNewUnitWrapper() {
-  static CUnit *unit;
-  __asm {
-    PUSHAD
-    MOV EBP, ESP
-    MOV unit, ESI
-  }
 
-  hooks::applyUpgradeFlagsToNewUnitHook(unit);
+	static CUnit* unit;
 
-  __asm {
-    POPAD
-    RETN
-  }
+	__asm {
+		MOV unit, ESI
+		PUSHAD
+	}
+
+	hooks::applyUpgradeFlagsToNewUnitHook(unit);
+
+	__asm {
+		POPAD
+		RETN
+	}
 }
 
+;
+
 void __declspec(naked) applyUpgradeFlagsToExistingUnitsWrapper() {
-  static CUnit *unit;
-  static u8 upgradeId;
-  __asm {
-    PUSHAD
-    MOV EBP, ESP
-    MOV ECX, [EBP + 36]  ;//8 - 4 + 32
-    MOV unit, ECX
-    MOV upgradeId, AL
-  }
 
-  hooks::applyUpgradeFlagsToExistingUnitsHook(unit->playerId, upgradeId);
+	static CUnit* unit;
+	static u8 upgradeId;
 
-  __asm {
-    POPAD
-    RETN 4
-  }
+	__asm {
+
+		PUSH EBP
+		MOV EBP, ESP
+
+		MOV upgradeId, AL
+
+		MOV EAX, [EBP+0x08]
+		MOV unit, EAX
+
+		PUSHAD
+
+	}
+
+	hooks::applyUpgradeFlagsToExistingUnitsHook(unit, upgradeId);
+
+	__asm {
+		POPAD
+		MOV ESP, EBP
+		POP EBP
+		RETN 4
+	}
 }
 
 } //Unnamed namespace
@@ -48,18 +59,22 @@ namespace hooks {
 const u32 Hook_ApplyUpgradeFlagsToExistingUnits = 0x00454540;
 
 void injectApplyUpgradeFlags() {
-  jmpPatch(applyUpgradeFlagsToNewUnitWrapper, 0x00454370);
-  jmpPatch(applyUpgradeFlagsToExistingUnitsWrapper, Hook_ApplyUpgradeFlagsToExistingUnits);
+	jmpPatch(applyUpgradeFlagsToNewUnitWrapper,			0x00454370, 2);
+	jmpPatch(applyUpgradeFlagsToExistingUnitsWrapper,	0x00454540, 2);
 }
 
-void applyUpgradeFlagsToExistingUnits(CUnit *unit, u8 upgradeId) {
-  __asm {
-    PUSHAD
-    PUSH unit
-    MOV AL, upgradeId
-    CALL Hook_ApplyUpgradeFlagsToExistingUnits
-    POPAD
-  }
+;
+
+void applyUpgradeFlagsToExistingUnits(CUnit* unit, u8 upgradeId) {
+
+	__asm {
+		PUSHAD
+		PUSH unit
+		MOV AL, upgradeId
+		CALL Hook_ApplyUpgradeFlagsToExistingUnits
+		POPAD
+	}
+
 }
 
 } //hooks
