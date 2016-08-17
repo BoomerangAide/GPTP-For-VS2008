55a2a58f0fe2256acc04ddc8df939f9cc9e6f8e0
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-08-17 13:23:22 +0200

    Update 3.999

--- GPTP/hooks/rally_point.cpp
+++ GPTP/hooks/rally_point.cpp
@@ -1,8 +1,6 @@
 //Contains hooks that control how rally points are used.
 
 #include "rally_point.h"
-#include "../SCBW/scbwdata.h"
-#include "../SCBW/enumerations.h"
 #include "../SCBW/api.h"
 
 namespace hooks {
@@ -12,34 +10,53 @@ namespace hooks {
 ///
 /// @param  unit      The unit that needs to receive rally orders.
 /// @param  factory   The unit (building) that created the given unit.
+///
+/// Identical to sub_466F50 @ 00466F50
+
 void orderNewUnitToRally(CUnit* unit, CUnit* factory) {
-  //Default StarCraft behavior
 
-  //Do nothing if the rally target is the factory itself or the rally target position is 0
-  if (factory->rally.unit == factory || !(factory->rally.pt.x)) return;
+	//Do nothing if the rally target is the factory itself or the rally target position is 0
+	if (
+		factory != NULL && 
+		factory->isFactory() && 
+		factory->rally.unit != factory && 
+		factory->rally.pt.x != 0
+	)
+	{
 
-  //Following should be allowed only on friendly units
-  if (factory->rally.unit && factory->rally.unit->playerId == unit->playerId)
-    unit->orderTo(OrderId::Follow, factory->rally.unit);
-  else
-    unit->orderTo(OrderId::Move, factory->rally.pt.x, factory->rally.pt.y);
+		//Following should be allowed only on friendly units
+		if (factory->rally.unit != NULL && (factory->rally.unit)->playerId == unit->playerId)
+			unit->orderTo(OrderId::Follow, factory->rally.unit);
+		else
+			unit->orderTo(OrderId::Move, factory->rally.pt.x, factory->rally.pt.y);
+
+	}
+
 }
+
+;
 
 /// Called when the player sets the rally point on the ground.
-void setRallyPosition(CUnit *unit, u16 x, u16 y) {
-  //Default StarCraft behavior
-  unit->rally.unit = NULL;
-  unit->rally.pt.x = x;
-  unit->rally.pt.y = y;
+void setRallyPosition(CUnit* unit, u16 x, u16 y) {
+	unit->rally.unit = NULL;
+	unit->rally.pt.x = x;
+	unit->rally.pt.y = y;
 }
 
+;
+
 /// Called when the player sets the rally point on a unit.
-void setRallyUnit(CUnit *unit, CUnit *target) {
-  //Default StarCraft behavior
-  if (!target) target = unit;
-  unit->rally.unit = target;
-  unit->rally.pt.x = target->getX();
-  unit->rally.pt.y = target->getY();
+void setRallyUnit(CUnit* unit, CUnit* target) {
+
+	if (target == NULL) 
+		target = unit;
+
+	unit->rally.unit = target;
+	unit->rally.pt.x = target->getX();
+	unit->rally.pt.y = target->getY();
+
 }
+
+;
 
 } //hooks
