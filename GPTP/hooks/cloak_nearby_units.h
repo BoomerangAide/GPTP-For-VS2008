3c2f9c1346a97a14f54f604f4abc0fc33d63c377
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-08-17 13:04:20 +0200

    Update 3.999

--- GPTP/hooks/cloak_nearby_units.h
+++ GPTP/hooks/cloak_nearby_units.h
@@ -1,11 +1,9 @@
 #pragma once
 #include <SCBW/structures/CUnit.h>
 
-//V241 for VS2008
-
 namespace hooks {
 
-void cloakNearbyUnitsHook(CUnit *unit);
+void cloakNearbyUnitsHook(CUnit* unit);
 
 void injectCloakNearbyUnits();
 
