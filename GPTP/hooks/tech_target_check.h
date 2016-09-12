f5bec39a7971006e13eff1443bf52d576db77c86
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-09-12 12:58:35 +0200

    Update 3.999

--- GPTP/hooks/tech_target_check.h
+++ GPTP/hooks/tech_target_check.h
@@ -1,17 +1,12 @@
 //Header file for the Tech Target Check hook module.
 #include <SCBW/structures/CUnit.h>
 
-//V241 for VS2008
-
 namespace hooks {
 
 //The hook function injected into StarCraft
-u16 getTechUseErrorMessageHook(const CUnit *target, u8 castingPlayer, u16 techId);
+u16 getTechUseErrorMessageHook(CUnit* target, u8 castingPlayer, u16 techId);
 
 //The injector function that should be called in initialize.cpp
 void injectTechTargetCheckHooks();
-
-//Native callable version of the hook
-u16 getTechUseErrorMessage(const CUnit *target, u8 castingPlayer, u16 techId);
 
 } //hooks
