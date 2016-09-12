59164bb95e8253a88d20c153da661b3aed130a80
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-09-12 12:57:05 +0200

    Update 3.999

--- GPTP/hooks/tech_target_check.cpp
+++ GPTP/hooks/tech_target_check.cpp
@@ -5,73 +5,110 @@
 
 //-------- Helper function declarations. Do NOT modify! --------//
 namespace {
-bool isInfestableUnit(const CUnit *unit);
+bool isInfestableUnit(CUnit* unit);
 } //Unnamed namespace
 
 namespace hooks {
 
-/// Test whether @p castingPlayer can use @p techId on the @p target.
-/// If successful, returns zero. If unsuccessful, returns the index of the
-/// appropriate error message string in stat_txt.tbl.
-u16 getTechUseErrorMessageHook(const CUnit *target, u8 castingPlayer, u16 techId) {
-  //Default StarCraft behavior
+//Should be equivalent to getSpellStatString @ 0x00491E80
+u16 getTechUseErrorMessageHook(CUnit* target, u8 castingPlayerId, u16 techId) {
 
-  if (target->stasisTimer)
-    return 900;         //Units in stasis can't be targeted.<0>
+	u16 stringId;
 
-  switch (techId) {
-    case TechId::Feedback:
-      if (units_dat::BaseProperty[target->id] & UnitProperty::Building)
-        return 877;     //Unable to target structure.<0>
-      if (!target->isValidCaster())
-        return 1330;    //Must target units with energy.<0>
-      break;
+	if(target->stasisTimer != 0)
+		stringId = 900; //Units in stasis can't be targeted.<0>
+	else
+	if(techId < TechId::DefensiveMatrix || techId > TechId::OpticalFlare)
+		stringId = 876; //Invalid target.<0>
+	else {
 
-    case TechId::MindControl:
-      if (target->playerId == castingPlayer)
-        return 1327;    //Must target enemy units<0>
-      if (units_dat::BaseProperty[target->id] & UnitProperty::Building)
-        return 877;     //Unable to target structure.<0>
-      if (target->id == UnitId::spider_mine
-          || target->id == UnitId::larva
-          || target->id == UnitId::egg
-          || target->id == UnitId::cocoon
-          || target->id == UnitId::interceptor
-          || target->id == UnitId::scarab
-          || target->id == UnitId::lurker_egg)
-        return 876;     //Invalid target.<0>
-      break;
+		//originally handled by switchs and internal arrays of id
 
-    case TechId::Hallucination:
-      if (target->id == UnitId::interceptor)
-        return 876;     //Invalid target.<0>
-      if (units_dat::BaseProperty[target->id] & UnitProperty::Building)
-        return 877;     //Unable to target structure.<0>
-      break;
+		if(techId == TechId::Feedback) {
 
-    case TechId::DefensiveMatrix:
-    case TechId::Irradiate:
-    case TechId::Restoration:
-    case TechId::OpticalFlare:
-      if (units_dat::BaseProperty[target->id] & UnitProperty::Building)
-        return 877;     //Unable to target structure.<0>
-      break;
+			if(units_dat::BaseProperty[target->id] & UnitProperty::Building)
+				stringId = 877; //Unable to target structure.<0>
+			else
+			if(!target->isValidCaster())
+				stringId = 1330; //Must target units with energy.<0>
+			else
+				stringId = 0; //valid target
 
-    case TechId::Infestation:
-      if (!isInfestableUnit(target))
-        return 870;     //Must target severely damaged Terran Command Center.<0>
-      break;
+		}
+		else
+		if(techId == TechId::MindControl) {
 
-    case TechId::Consume:
-      if (units_dat::BaseProperty[target->id] & UnitProperty::Building
-          || target->playerId != castingPlayer
-          || target->getRace() != RaceId::Zerg
-          || target->id == UnitId::larva)
-        return 897;     //Invalid target.<0>
-      break;
-  }
+			if(target->playerId == castingPlayerId)
+				stringId = 1327; //Must target enemy units<0>
+			else
+			if(units_dat::BaseProperty[target->id] & UnitProperty::Building)
+				stringId = 877; //Unable to target structure.<0>
+			else
+			if(target->id < UnitId::TerranVultureSpiderMine || target->id > UnitId::ZergLurkerEgg)
+				stringId = 0; //valid target
+			else
+			if(	target->id == UnitId::TerranVultureSpiderMine ||
+				target->id == UnitId::ZergLarva ||
+				target->id == UnitId::ZergEgg ||
+				target->id == UnitId::ZergCocoon ||
+				target->id == UnitId::ProtossInterceptor ||
+				target->id == UnitId::ProtossScarab ||
+				target->id == UnitId::ZergLurkerEgg
+			)
+				stringId = 876; //Invalid target.<0>
+			else
+				stringId = 0; //valid target
 
-  return 0;             //No error
+		}
+		else
+		if(techId == TechId::Hallucination) {
+			if(target->id == UnitId::ProtossInterceptor)
+				stringId = 876; //Invalid target.<0>
+			else
+			if(units_dat::BaseProperty[target->id] & UnitProperty::Building)
+				stringId = 877; //Unable to target structure.<0>
+			else
+				stringId = 0; //valid target
+		}
+		else
+		if(	techId == TechId::DefensiveMatrix ||
+			techId == TechId::Irradiate ||
+			techId == TechId::Restoration ||
+			techId == TechId::OpticalFlare
+		)
+		{
+			if(units_dat::BaseProperty[target->id] & UnitProperty::Building)
+				stringId = 877; //Unable to target structure.<0>
+			else
+				stringId = 0; //valid target
+		}
+		else
+		if(techId == TechId::Infestation) {
+			if(!isInfestableUnit(target))
+				stringId = 870;	//Must target severely damaged Terran Command Center.<0>
+			else
+				stringId = 0; //valid target
+		}
+		else
+		if(techId == TechId::Consume) {
+			if(
+				units_dat::BaseProperty[target->id] & UnitProperty::Building ||
+				target->playerId != castingPlayerId ||
+				target->getRace() != RaceId::Zerg ||
+				target->id == UnitId::ZergLarva
+			)
+				stringId = 876; //Invalid target.<0>
+			else
+				stringId = 0; //valid target
+
+		}
+		else
+			stringId = 876; //Invalid target.<0>
+
+	}
+
+	return stringId;
+
 }
 
 } //hooks
@@ -80,19 +117,20 @@ u16 getTechUseErrorMessageHook(const CUnit *target, u8 castingPlayer, u16 techId
 namespace {
 
 const u32 Func_IsInfestable = 0x00402210;
-bool isInfestableUnit(const CUnit *unit) {
+bool isInfestableUnit(CUnit* unit) {
 
-  static Bool32 result;
-  
-  __asm {
-    PUSHAD
-    MOV EDX, unit
-    CALL Func_IsInfestable
-    MOV result, EAX
-    POPAD
-  }
+	static Bool32 result;
 
-  return result != 0;
+	__asm {
+		PUSHAD
+		MOV EDX, unit
+		CALL Func_IsInfestable
+		MOV result, EAX
+		POPAD
+	}
+
+	return result != 0;
 
 }
+
 }
