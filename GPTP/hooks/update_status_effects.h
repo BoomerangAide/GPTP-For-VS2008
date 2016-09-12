76d6efdc72ca5ea37a46db31265147875d070ad6
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-09-12 13:22:36 +0200

    Update 3.999

--- GPTP/hooks/update_status_effects.h
+++ GPTP/hooks/update_status_effects.h
@@ -1,15 +1,10 @@
 #pragma once
 #include <SCBW/structures/CUnit.h>
 
-//V241 for VS2008
-
 namespace hooks {
 
-void updateStatusEffectsHook(CUnit *unit);
+void updateStatusEffects(CUnit* unit);
 
 void injectUpdateStatusEffects();
-
-//Wrapper for native version of updateStatusEffects()
-void updateStatusEffects(CUnit *unit);
 
 } //hooks
