700c1688ca343dab1a63cfc83f207b1181a813ef
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-09-12 13:15:17 +0200

    Update 3.999

--- GPTP/hooks/transfer_tech_upgrades.h
+++ GPTP/hooks/transfer_tech_upgrades.h
@@ -1,13 +1,13 @@
 //Header file for the Transfer Tech & Upgrades hook module.
 #include <SCBW/structures/CUnit.h>
 
-//V241 for VS2008
-
 namespace hooks {
 
-void transferUnitTechToPlayerHook(const CUnit *source, u8 targetPlayerId);
-void transferUnitUpgradesToPlayerHook(const CUnit *source, u8 targetPlayerId);
-void applyUnitUpgradeFlagsToAllFriendlyUnitsHook(CUnit *unit);
+void transferUnitTechToPlayerHook(CUnit* sourceUnit, u8 targetPlayerId, u32 researchTechFunc = 0x0047B3C0);	//0x004E2C00
+
+//Default value for researchUpgradesFunc is set as increaseUpgradeLevel @ 0047B340
+//The other possible value is sub_47B940 @ 0047B940 (called by GiveAllUnitPropertiesToPlayerAndMakeIdle)
+void transferUnitUpgradesToPlayerHook(CUnit* sourceUnit, u8 targetPlayerId, u32 researchUpgradesFunc = 0x0047B340);		//0x004E2B50
 
 void injectTransferTechAndUpgradesHooks();
 
