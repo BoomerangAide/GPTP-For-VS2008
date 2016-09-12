53a52ccc7a93d15d6cd720581d4fd3594d88a235
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-09-12 13:19:15 +0200

    Update 3.999

--- GPTP/hooks/unit_speed.cpp
+++ GPTP/hooks/unit_speed.cpp
@@ -4,60 +4,85 @@
 #include "../SCBW/enumerations.h"
 #include "../SCBW/scbwdata.h"
 
-//V241 for VS2008
-
 namespace hooks {
 
 /// Calculates the unit's modified movement speed, factoring in upgrades and status effects.
 ///
 /// @return		The modified speed value.
-u32 getModifiedUnitSpeedHook(const CUnit* unit, u32 baseSpeed) {
-	//Default StarCraft behavior
+/// Logically equivalent to GetModifiedUnitSpeed @ 0047B5F0  
+u32 getModifiedUnitSpeedHook(CUnit* unit, u32 baseSpeed) {
+
+
 	u32 speed = baseSpeed;
+
 	int speedModifier = (unit->stimTimer ? 1 : 0) - (unit->ensnareTimer ? 1 : 0)
                       + (unit->status & UnitStatus::SpeedUpgrade ? 1 : 0);
+
 	if (speedModifier > 0) {
+
 		if (unit->id == UnitId::scout || unit->id == UnitId::Hero_Mojo || unit->id == UnitId::Hero_Artanis)
-      speed = 1707;
+			speed = 1707;
 		else {
-			speed += speed >> 1;
+
+			speed += (speed / 2);
+
 			if (speed < 853)
-        speed = 853;
+				speed = 853;
+
 		}
+
 	}
-	else if (speedModifier < 0)
-		speed >>= 1;
+	else
+	if (speedModifier < 0)
+		speed /= 2;
+
 	return speed;
+
 }
 
 /// Calculates the unit's acceleration, factoring in upgrades and status effects.
 ///
 /// @return		The modified acceleration value.
-u32 getModifiedUnitAccelerationHook(const CUnit* unit) {
-	//Default StarCraft behavior
+/// Logically equivalent to getModifiedUnitAcceleration @ 0047B8A0 
+u32 getModifiedUnitAccelerationHook(CUnit* unit) {
+
+
 	u32 acceleration = flingy_dat::Acceleration[units_dat::Graphic[unit->id]];
+
 	int modifier = (unit->stimTimer ? 1 : 0) - (unit->ensnareTimer ? 1 : 0)
                  + (unit->status & UnitStatus::SpeedUpgrade ? 1 : 0);
+
 	if (modifier > 0)
-		acceleration <<= 1;
-	else if (modifier < 0)
-		acceleration -= acceleration >> 2;
+		acceleration *= 2;
+	else 
+	if (modifier < 0)
+		acceleration = acceleration - (acceleration / 4);
+
 	return acceleration;
+
+
 }
 
 /// Calculates the unit's turn speed, factoring in upgrades and status effects.
 ///
 /// @return		The modified turning speed value.
-u32 getModifiedUnitTurnSpeedHook(const CUnit* unit) {
-	//Default StarCraft behavior
+/// Logically equivalent to getModifiedUnitTurnRadius @ 0047B850 
+u32 getModifiedUnitTurnSpeedHook(CUnit* unit) {
+
+
 	u32 turnSpeed = flingy_dat::TurnSpeed[units_dat::Graphic[unit->id]];
+
 	int modifier = (unit->stimTimer ? 1 : 0) - (unit->ensnareTimer ? 1 : 0)
                  + (unit->status & UnitStatus::SpeedUpgrade ? 1 : 0);
+
 	if (modifier > 0)
-		turnSpeed <<= 1;
+		turnSpeed *= 2;
+
 	else if (modifier < 0)
-		turnSpeed -= turnSpeed >> 2;
+		turnSpeed = turnSpeed - (turnSpeed / 4);
+
 	return turnSpeed;
+
 }
 
 } //hooks
