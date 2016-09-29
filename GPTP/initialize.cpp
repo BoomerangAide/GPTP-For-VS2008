50b760c1922042ada7e0fbf198d57eefa9c9e034
UndeadStar <BoomerangAide@users.noreply.github.com>
2016-09-29 14:39:46 +0200

    Update V4.001

--- GPTP/initialize.cpp
+++ GPTP/initialize.cpp
@@ -39,6 +39,7 @@
 #include "hooks/orders/building_making/make_nydus_exit.h"
 #include "hooks/orders/unit_making/unit_morph.h"
 #include "hooks/interface/wireframe.h"
+#include "hooks/orders/building_making/building_terran.h"
 
 #include "hooks/orders/base_orders/attack_orders.h"
 #include "hooks/interface/buttonsets.h"
@@ -97,6 +98,7 @@ BOOL WINAPI Plugin::InitializePlugin(IMPQDraftServer *lpMPQDraftServer) {
 	hooks::injectUnitMorphHooks();
 	hooks::injectWireframeHook();
 	hooks::injectDieOrderHook();
+	hooks::injectBuildingTerranHook();
 
 	hooks::injectApplyUpgradeFlags();
 	hooks::injectAttackPriorityHooks();
