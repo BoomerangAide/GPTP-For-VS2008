afe96b3d393e4e23d230cc62ce3e340fe1f7d572
UndeadStar <BoomerangAide@users.noreply.github.com>
2017-06-22 14:11:55 +0200

    Version 4.011

--- GPTP/hooks/burrow_tech_inject.cpp
+++ GPTP/hooks/burrow_tech_inject.cpp
@@ -72,9 +72,9 @@ namespace {
 namespace hooks {
 
 	void injectBurrowTechHooks() {
-		jmpPatch(CMDRECV_UnburrowWrapper,	0x004E8C20, 3);
-		jmpPatch(CMDRECV_BurrowWrapper,		0x004EA180, 6);
-		jmpPatch(Unburrow_ProcWrapper,		0x004EA3E0, 1);
+		jmpPatch(CMDRECV_UnburrowWrapper,	0x004C1AC0, 3);
+		jmpPatch(CMDRECV_BurrowWrapper,		0x004C1FA0, 6);
+		jmpPatch(Unburrow_ProcWrapper,		0x004E97C0, 1);
 	}
 
 } //hooks
