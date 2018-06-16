43efcbda6541abc22b4683d1a3a1db1de39818e3
UndeadStar <BoomerangAide@users.noreply.github.com>
2018-06-16 12:02:28 +0200

    Version 4.100

--- GPTP/hooks/utils/utils.h
+++ GPTP/hooks/utils/utils.h
@@ -1,10 +1,11 @@
 #pragma once
-#include "../../SCBW/structures/CUnit.h"
+#include "../../SCBW/structures.h"
 
 namespace hooks {
 
-bool unit_IsStandardAndMovable(CUnit* unit);	//0047B770
-CUnit* getActivePlayerFirstSelection();			//0049A850
+void InitializeDialog(BinDlg* dialog, void* dlgFunc);	//00419D20
+bool unit_IsStandardAndMovable(CUnit* unit);			//0047B770
+CUnit* getActivePlayerFirstSelection();					//0049A850
 
 void injectUtilsHooks();
 
