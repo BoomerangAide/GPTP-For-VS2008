4b06f53fae955b54bd51405089bcbf0f0d99777e
UndeadStar <BoomerangAide@users.noreply.github.com>
2018-06-18 20:49:02 +0200

    Version 4.101

--- GPTP/hooks/interface/buttonsets.h
+++ GPTP/hooks/interface/buttonsets.h
@@ -3,11 +3,13 @@
 
 namespace hooks {
 
-void updateButtonSetEx();			//00458BC0  updateButtonSetEx
-void updateButtonSet_Sub458D50();	//00458D50  sub_458D50
-void updateButtonSet();				//00458DE0  updateButtonSet
-void updateButtonSet_Sub4591D0();	//004591D0  sub_4591D0
-void updateCurrentButtonset();		//004599A0  updateCurrentButtonset
+void updateButtonSetEx();						//00458BC0  updateButtonSetEx
+void updateButtonSet_Sub458D50();				//00458D50  sub_458D50
+void updateButtonSet();							//00458DE0  updateButtonSet
+void updateButtonSet_Sub4591D0();				//004591D0  sub_4591D0
+void updateCurrentButtonset();					//004599A0  updateCurrentButtonset
+
+void statbtn_BIN_CustomCtrlID(BinDlg* dialog);	//00459AD0 statbtn_BIN_CustomCtrlID
 
 BUTTON_SET* getButtonSet(int index);
 
